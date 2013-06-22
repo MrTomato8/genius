36c3e3b3b8ab0eaa1dc128ec49fa92ebbf1da53e
Sergej Alikov <sergej.alikov@gmail.com>
2013-06-22 18:00:45 +0300

    add pagination

--- gap/apps/pricelist/views.py
+++ gap/apps/pricelist/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from apps.pricelist.utils import import_csv
 from apps.pricelist.models import Price
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # TODO:allow staff only
 def import_pricelist(request):
@@ -23,8 +24,21 @@ def import_pricelist(request):
 
 # TODO: this one needs pagination
 def list(request):
-    prices = Price.objects.filter(~Q(state='inactive'))
+    prices_all = Price.objects.filter(~Q(state='inactive'))
+    paginator = Paginator(prices_all, 20)
+
+    page = request.GET.get('page')
+    try:
+        prices = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        prices = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        prices = paginator.page(paginator.num_pages)
     return render(request, 'pricelist/list.html', {
         'prices': prices,
+        'paginator': paginator,
+        'page_obj': prices,
         'title': 'Active Prices',
     })
