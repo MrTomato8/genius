7052b685d71e6eb2fed326b7651f898a228c13c5
Sergej Alikov <sergej.alikov@gmail.com>
2013-06-29 02:19:53 +0300

    Vectors for the future

--- gap/apps/options/views.py
+++ gap/apps/options/views.py
@@ -1,8 +1,29 @@
 from django.views.generic.base import TemplateView
+from django.db import models
+
+Product = models.get_model('catalogue', 'Product')
+Option = models.get_model('catalogue', 'Option')
+OptionChoice = models.get_model('pricelist', 'OptionChoice')
+Price = models.get_model('pricelist', 'Price')
 
 
 class PickOptionsView(TemplateView):
     template_name = 'options/pick.html'
 
     def get_context_data(self, **kwargs):
-        return {'msg': 'Hello World'}
+
+        context = super(PickOptionsView, self).get_context_data(**kwargs)
+        context['product'] = Product.objects.get(pk=context['params']['pk'])
+        context['opts'] = []
+
+        # TODO: check if product has ptions assigned, or productclass has
+        # options assigned
+
+        for o in Option.objects.all():
+            c = OptionChoice.objects.filter(
+                option=o, prices__in=Price.objects.filter(
+                    product=context['product'])).distinct()
+
+            context['opts'].append({'option': o, 'choices': c})
+
+        return context
