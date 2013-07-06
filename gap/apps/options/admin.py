ce2448710a2bdcf05e2e9f12c02d5ae52f61b688
Sergej Alikov <sergej.alikov@gmail.com>
2013-07-06 18:33:27 +0300

    Different approacht to filtering bound options
    Which doesn't filter current option selection.

--- gap/apps/options/admin.py
+++ gap/apps/options/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
 from apps.options.models import OptionPicker, OptionPickerGroup
 from django.db import models
+from django import forms
 
 Option = models.get_model('catalogue', 'Option')
 
 
-class OptionPickerAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'option':
-            opts = map(lambda x: x.option.pk,
-                       OptionPicker.objects.all())
+class OptionPickerForm(forms.ModelForm):
 
-            kwargs['queryset'] = Option.objects.exclude(pk__in=opts)
-        return super(OptionPickerAdmin, self).formfield_for_foreignkey(
-            db_field, request, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(OptionPickerForm, self).__init__(*args, **kwargs)
+
+        opts = map(lambda x: x.option.pk,
+                   OptionPicker.objects.exclude(option=self.instance.option))
+
+        self.fields['option'].queryset = Option.objects.exclude(pk__in=opts)
+
+
+class OptionPickerAdmin(admin.ModelAdmin):
+    form = OptionPickerForm
+
 
 admin.site.register(OptionPicker, OptionPickerAdmin)
 admin.site.register(OptionPickerGroup)
