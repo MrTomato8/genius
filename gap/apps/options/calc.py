babea5ba5c5ebceaa26a11237c68499b3d47462d
Sergej Alikov <sergej.alikov@gmail.com>
2013-07-27 23:12:22 +0300

    Fix discrete pricing logic and rounding. Refactor

--- gap/apps/options/calc.py
+++ gap/apps/options/calc.py
@@ -1,6 +1,9 @@
 from django.db.models import Max
 from apps.options import utils
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
+from django.core.exceptions import ObjectDoesNotExist
+
+TWOPLACES = Decimal(10) ** -2
 
 
 class OptionsCalculatorError(Exception):
@@ -22,6 +25,7 @@ class PriceNotAvailable(Exception):
 class CalculatedPrices:
     def __init__(self):
         self._prices = {}
+        self.discrete_pricing = False
 
     def __len__(self):
         return len(self._prices)
@@ -90,6 +94,34 @@ class BaseOptionsCalculator:
         # Return prices for all found discrete quantities
         return prices
 
+    def _apply_choice_data(self, price, choices, choice_data):
+        if utils.custom_size_chosen(choices):
+            try:
+                cargs = choice_data[utils.custom_size_option_name()]
+            except KeyError:
+                raise NotEnoughArguments(
+                    'choice_data argument does not contain {0} key'.format(
+                        utils.custom_size_option_name()))
+
+            if 'width' in cargs and 'height' in cargs:
+                # calculate area in square metres
+                area = Decimal(cargs['width'] * cargs['height']) / Decimal(1000000)
+
+                return price * area
+
+            else:
+                raise NotEnoughArguments('For custom size width and height'
+                                         'should be supplied')
+
+        # More transformations may be added here in the future
+        return price
+
+    def _total(self, price, quantity):
+        return (price * quantity).quantize(TWOPLACES, ROUND_HALF_UP)
+
+    def _unit(self, price, quantity):
+        return (price / quantity).quantize(TWOPLACES, ROUND_HALF_UP)
+
     def calculate_costs(self, choices, quantity=None, choice_data=None):
         '''
         Returns CalculatedPrices object
@@ -98,78 +130,71 @@ class BaseOptionsCalculator:
         height arguments into account. Width and height units are millimeters
         and price value is per square metre for this case.
         '''
+        # Totals (price*quantity) are recalculated to make price
+        # consistent with basket's price calculation. Basket stores only
+        # unit prices with 2 decimal places, so on the calculation one cent may be
+        # lost. Here we just need to adapt to Oscar's way of calculating things.
         result = CalculatedPrices()
 
         if choice_data is None:
             choice_data = {}
 
-        TWOPLACES = Decimal(10) ** -2
-
         prices = self.pick_prices(choices, quantity)
 
-        for price in prices:
+        # Discrete pricing scheme
+        if len(prices) > 1:
+            result.discrete_pricing = True
 
-            rpl_price = price.rpl_price
-            tpl_price = price.tpl_price
+            for price in prices:
 
-            if utils.custom_size_chosen(choices):
-                try:
-                    cargs = choice_data[utils.custom_size_option_name()]
-                except KeyError:
-                    raise NotEnoughArguments(
-                        'choice_data argument does not contain {0} key'.format(
-                            utils.custom_size_option_name()))
+                rpl_price = self._apply_choice_data(
+                    price.rpl_price, choices, choice_data)
+                tpl_price = self._apply_choice_data(
+                    price.tpl_price, choices, choice_data)
 
-                if 'width' in cargs and 'height' in cargs:
-                    # calculate area in square metres
-                    area = Decimal(cargs['width'] * cargs['height']) / Decimal(1000000)
+                rpl_unit_price = self._unit(rpl_price, price.quantity)
+                tpl_unit_price = self._unit(tpl_price, price.quantity)
 
-                    rpl_price = rpl_price * area
-                    tpl_price = tpl_price * area
-                else:
-                    raise NotEnoughArguments('For custom size width and height'
-                                             'should be supplied')
-
-            # In pricelist price may be supplied for multiple
-            # units (price.quantity).
-            # For price calculation we need unit price.
-
-            rpl_unit_price = (rpl_price / price.quantity).quantize(TWOPLACES)
-            tpl_unit_price = (tpl_price / price.quantity).quantize(TWOPLACES)
-
-            # Unit price may be different for dfferent quantities
-            # of the same choice set. That is why we store them separately
-
-            # Totals (price*quantity) are recalculated to make price
-            # consistent with basket's price calculation. Basket stores only
-            # unit prices with 2 decimal places, so on the calculation one cent may be
-            # lost. Here we just need to adapt to Oscar's way of calculating things.
-            if quantity is not None:
                 price_data = {}
 
-                price_data['rpl_price_incl_tax'] = (
-                    rpl_unit_price * quantity).quantize(TWOPLACES)
-                price_data['tpl_price_incl_tax'] = (
-                    tpl_unit_price * quantity).quantize(TWOPLACES)
-
-                # These are already quantized
-                price_data['rpl_unit_price_incl_tax'] = rpl_unit_price
-                price_data['tpl_unit_price_incl_tax'] = tpl_unit_price
-
-                result.add(quantity, price_data)
-            else:
-                price_data = {}
-
-                price_data['rpl_price_incl_tax'] = (
-                    rpl_unit_price * price.quantity).quantize(TWOPLACES)
-                price_data['tpl_price_incl_tax'] = (
-                    tpl_unit_price * price.quantity).quantize(TWOPLACES)
+                price_data['rpl_price_incl_tax'] = self._total(
+                    rpl_unit_price, price.quantity)
+                price_data['tpl_price_incl_tax'] = self._total(
+                    tpl_unit_price, price.quantity)
 
                 # These are already quantized
                 price_data['rpl_unit_price_incl_tax'] = rpl_unit_price
                 price_data['tpl_unit_price_incl_tax'] = tpl_unit_price
 
                 result.add(price.quantity, price_data)
+        else:
+            result.discrete_pricing = False
+            try:
+                price = prices.get()
+            except ObjectDoesNotExist:
+                return result
+
+            if quantity is not None:
+                rpl_price = self._apply_choice_data(
+                    price.rpl_price, choices, choice_data)
+                tpl_price = self._apply_choice_data(
+                    price.tpl_price, choices, choice_data)
+
+                rpl_unit_price = self._unit(rpl_price, price.quantity)
+                tpl_unit_price = self._unit(tpl_price, price.quantity)
+
+                price_data = {}
+
+                price_data['rpl_price_incl_tax'] = self._total(
+                    rpl_unit_price, quantity)
+                price_data['tpl_price_incl_tax'] = self._total(
+                    tpl_unit_price, quantity)
+
+                # These are already quantized
+                price_data['rpl_unit_price_incl_tax'] = rpl_unit_price
+                price_data['tpl_unit_price_incl_tax'] = tpl_unit_price
+
+                result.add(quantity, price_data)
 
         return result
 
