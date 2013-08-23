2dae26e8023a863bf3f002e82ce1f14769f516a1
Sergej Alikov <sergej.alikov@gmail.com>
2013-08-23 13:47:56 +0300

    Convenient managment of conflicting choices

--- gap/apps/options/admin.py
+++ gap/apps/options/admin.py
@@ -27,6 +27,7 @@ class OptionPickerAdmin(admin.ModelAdmin):
 
 
 class OptionChoiceAdmin(admin.ModelAdmin):
+    filter_horizontal = ('conflicts_with',)
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
             return self.readonly_fields + ('code',)
