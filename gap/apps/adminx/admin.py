31e9a15c30e135816e3d88bc3b46762e0a68655f
Sergej Alikov <sergej.alikov@gmail.com>
2013-08-26 10:37:34 +0300

    bring back filter to user_permisions

--- gap/apps/adminx/admin.py
+++ gap/apps/adminx/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 class MyUserAdmin(UserAdmin):
-    filter_horizontal = ('groups',)
+    filter_horizontal = ('groups', 'user_permissions')
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
