509b988a9e34bd7aee42686fa0e621c539b3013d
zauddelig <zauddelig@gmail.com>
2013-09-30 09:47:06 +0200

    boolean fix

--- gap/apps/options/calc.py
+++ gap/apps/options/calc.py
@@ -340,7 +340,7 @@ class BaseOptionsCalculator:
                     price.tpl_price, choices, choice_data)
                 
                 items_per_pack = price.items_per_pack
-                if not matrix_for_pack or quantity is not None:
+                if not matrix_for_pack and quantity is not None:
                     nr_of_units = self._calc_units(
                         items_per_pack, quantity)
                 elif quantity is None:
