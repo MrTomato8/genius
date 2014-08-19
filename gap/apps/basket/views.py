161573a020964bf5c64b977e7b41fcbe472f09df
vladimir.lyukov <v.lyukov@gmail.com>
2014-08-19 14:04:40 +0400

    Implemented "Remove" button in quotebuilder

--- gap/apps/basket/views.py
+++ gap/apps/basket/views.py
@@ -1,6 +1,25 @@
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import View
 from oscar.apps.basket.views import BasketView as CoreBasketView
+
 from apps.basket.forms import BasketLineForm
+from apps.basket.models import Line
+
 
 class BasketView(CoreBasketView):
     form_class = BasketLineForm
     pass
+
+
+class RemoveItemView(View):
+    def post(self, request, line_id):
+        try:
+            line = Line.objects.get(pk=line_id)
+            msg = 'Removed "%s" from basket' % line.product_and_options_description()
+            line.delete()
+            messages.add_message(request, messages.SUCCESS, msg)
+        except Line.DoesNotExist:
+            messages.add_message(request, messages.ERROR, 'Product not found in basket')
+        return HttpResponseRedirect(request.REQUEST.get('next', reverse('basket:summary')))
