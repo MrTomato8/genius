d66f1c2f4ba3ccf88e18eb6af0fee74644e7ced9
vladimir.lyukov <v.lyukov@gmail.com>
2014-08-21 14:43:59 +0400

    Better #quoteform submittion JS

--- gap/static/local/js/pick.js
+++ gap/static/local/js/pick.js
@@ -1,8 +1,11 @@
 PPS.pickForm = {
     init: function() {
-        var calculared = false;
-
-        $('.panel-default.last .option-choice .radio-choice').click(function () {
+        $('#pickoptions .panel-default .radio-choice').click(function () {
+            console.log('wat');
+            if ($('#pickoptions .panel-default:not(:has(.radio-choice:checked))').length) {
+                // Not all options selected
+                return;
+            }
             $.ajax({
                 data: $('form#pickoptions').serialize(),
                 type: $('form#pickoptions').attr('method'),
@@ -20,36 +23,12 @@ PPS.pickForm = {
                         $('#modalchangepw').html(res);
                         $('#form-errors').html('');
                     }
+                    $('#id_quantity').trigger("keyup");
                 }
             });
-            calculared = true;
-            return false;
         });
 
-        $('.option-choice .radio-choice').click(function () {
-            if (calculared) {
-                var quantity = $('#quantity_picker input:checked').attr('value');
-                $.ajax({
-                    data: $('form#pickoptions').serialize(),
-                    type: $('form#pickoptions').attr('method'),
-                    url: $('form#pickoptions').attr('action'),
-                    success: function (response) {
-                        x = $('<div></div>');
-                        x.html(response);
-                        res = x.find('#getquote_wrap');
-                        if (!res.html()) {
-                            res = x.find('#form-errors');
-                            $('#form-errors').html(res);
-                            $('#modalchangepw').html('');
-                        }
-                        else {
-                            $('#modalchangepw').html(res);
-                            $('#form-errors').html('');
-                        }
-                        $('li[data-qty="' + quantity + '"]').trigger("click");
-                    }
-                });
-            }
+        $('#pickoptions .radio-choice').click(function () {
             prev = $(this).first().closest('.panel-collapse');
             prev.collapse('toggle').siblings('a.panel-heading').addClass('collapsed');
             next = prev.parent('.panel').next();
@@ -59,7 +38,7 @@ PPS.pickForm = {
             PPS.pickForm.refreshSelectedChoicesList()
         });
 
-        $(".radio-choice").click(disableConflictingChoices);
+        $("#pickoptions .radio-choice").click(disableConflictingChoices);
         this.refreshSelectedChoicesList();
     },
 
