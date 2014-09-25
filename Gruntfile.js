73b456c43ad1e3a2e148d8221ae3174b95414228
Denys Butenko <ua13dark@gmail.com>
2014-09-25 23:35:19 +0300

    Remove large compiled main.css. Run 'grunt build' to compile it.
    Decrease compiled size 140.45kB → 116.39kB

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,53 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    less: {
-      development: {
-        options: {
-          paths: ["gap/static/local/less"]
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        less: {
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "gap/static/local/css/main.css": "gap/static/local/less/main.less"
+                }
+            },
+            development: {
+                options: {
+                    paths: ["gap/static/local/less"]
+                },
+                files: {
+                    "gap/static/local/css/main.css": "gap/static/local/less/main.less"
+                }
+            }
         },
-        files: {
-          "gap/static/local/css/main.css": "gap/static/local/less/main.less"
+        watch: {
+            options: {
+                livereload: true
+            },
+            less: {
+                files: '**/*.less',
+                tasks: ['less:development']
+            },
+            html: {
+                files: '**/*.html'
+            }
         }
-      }
-    },
-    watch: {
-      options: {
-        livereload: true,
-      },
-      less: {
-        files: '**/*.less',
-        tasks: ['less:development']
-      },
-      html: {
-        files: '**/*.html',
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['less:development']);
+    // Default task.
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production']);
 
 };
