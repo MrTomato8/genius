11a1bee44ebfe18def454ef3139928e7d735425a
zauddelig <zauddelig@gmail.com>
2014-10-01 10:43:12 +0200

    Update urls.py

--- gap/apps/pricelist/urls.py
+++ gap/apps/pricelist/urls.py
@@ -5,6 +5,7 @@ from apps.pricelist.views import CSVUpdate,CSVList
 urlpatterns = patterns(
     'apps.pricelist.views',
     (r'^import', 'import_pricelist'),
-    url(r'^list/(?P<pk>\S+)', CSVUpdate.as_view(),name='csvupdate'),
-    url(r'^list', CSVList.as_view(),name='csvview'),
+    url(r'^list/(?P<pk>\S+)/', CSVUpdate.as_view(),name='csvupdate'),
+    url(r'^list/', CSVList.as_view(),name='csvview'),
+    url(r'^500.html', CSVList.as_view(),name='err'),
 )
