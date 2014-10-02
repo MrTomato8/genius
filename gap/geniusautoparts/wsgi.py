3fb2699193a0eb3d1e495501e6ecc4a74bcfed33
Denys Butenko <ua13dark@gmail.com>
2014-10-02 17:47:27 +0300

    Fix wsgi.py

--- gap/geniusautoparts/wsgi.py
+++ gap/geniusautoparts/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-path = '/home/geniusautoparts/public_html/gap'
+path = '/home/skillfullabs.com/public_html/gap'
 if path not in sys.path:
     sys.path.append(path)
 
