c5fec7fbdd1802e230f389fb45163fefdf221550
Vlad <tmlwyb@gmail.com>
2014-10-14 15:48:33 +0300

    category and product admin pages: description field with editor

--- gap/apps/catalogue/admin.py
+++ gap/apps/catalogue/admin.py
@@ -1,1 +1,17 @@
-import oscar.apps.catalogue.admin
+from django.contrib import admin
+from apps.catalogue.models import Category, Product
+from geniusautoparts.settings import TINYMCE_PATH
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    class Media:
+        js = TINYMCE_PATH
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Media:
+        js = TINYMCE_PATH
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
